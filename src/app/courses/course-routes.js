0a6e8953cd499e9f3f47ed476a186dc56d2a5a11
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2020-03-12 17:18:51 +0200

    test

--- src/app/courses/course-routes.js
+++ src/app/courses/course-routes.js
@@ -5,5 +5,5 @@ export default [
     { path: '', redirect: { name: 'course-list' } },
     { path: '/courses', name: "course-list", component: CourseList },
     { path: '/create-course', name: "course-create", component: CourseCreate },
-    { path: '/courses/:id', name: 'detail' }
+    { path: '/courses/:id', name: 'detail', component: CourseList }
 ]
