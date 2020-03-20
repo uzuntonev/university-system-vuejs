e25e5e6aaef76e916e55d3ce1bcb8844240be714
Georgi Uzuntonev <georgi.uzuntonev@gmail.com>
2020-03-20 19:19:12 +0200

    fixed bug import firebase

--- src/app/shared/services/firebaseConfig.js
+++ src/app/shared/services/firebaseConfig.js
@@ -1,4 +1,6 @@
-import firebase from 'firebase';
+import * as firebase from "firebase";
+import "firebase/app";
+import "firebase/storage";
 
 var firebaseConfig = {
   apiKey: 'AIzaSyBofdd4FPGA1KZjPVqtT7FL6eXsv18sggM',
